1e1301592191af1b289921aca87492a30aa7e75a
Karma Soliman <karma.soliman@epita.fr>
2026-02-21 14:59:20 +0100

    fix: completing .env.example. edit type of database to postgresql

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ import dotenv from "dotenv"
 dotenv.config()
 const config = {
   port: process.env.PORT || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
-  databaseUrl: process.env.DATABASE_URL || "./database.sqlite",
+  nodeEnv: process.env.NODE_ENV ,
 }
